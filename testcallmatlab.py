518b3194360a66db2c7f34c43d1d2d04f0523e37
kebabnguyen <34109891+kebabnguyen@users.noreply.github.com>
2022-02-03 12:29:21 -0600

    WIP for power detect
    focus on streamlining processes to just running one script

---
+++ testcallmatlab.py
@@ -0,0 +1,6 @@
+import matlab.engine
+eng=matlab.engine.start_matlab()
+#might want to convert the ppbinarytest into a function format
+#feed it the filename
+#output a floor value for detection, and i guess the decision vector?
+#i dont think we'll need the overall fft; we just want to know where there's a signal or not
