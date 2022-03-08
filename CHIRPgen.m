f7b32e8def5be021e6f242f7b66ad17b161c3800
womandalorian <93218734+womandalorian@users.noreply.github.com>
2022-03-08 12:14:07 -0600

    generates baseband lfm and writes to text files

---
+++ CHIRPgen.m
@@ -0,0 +1,39 @@
+%% SINGLE BASEBAND CHIRP
+clear
+close all
+
+% Initializations
+
+T = 50e-6; % pulsewidth
+BT = 50; % time-bandwidth product
+B = BT/T; % swept bandwidth
+OVS = 10; % oversampling factor
+N = BT*OVS; % number of samples
+t_s = linspace(0,T,N); % time vector
+
+% Baseband chirp generation
+
+s = exp(1i*pi*(B/T)*t_s.^2); % LFM
+s = s/norm(s); % power normalizing
+
+% Writing binary to text file
+
+fileID_re = fopen('CHIRPgen_re.txt','W');
+fwrite(fileID_re,real(s),'float');
+fclose(fileID_re);
+
+fileID_im = fopen('CHIRPgen_im.txt','W');
+fwrite(fileID_im,imag(s),'float');
+fclose(fileID_im);
+
+% Graph
+
+figure(1)
+plot(t_s/T,real(s))
+hold on
+plot(t_s/T,imag(s))
+hold off
+legend('\Re(s)','\Im(s)')
+xlabel('Normalized Time (1 \cdot T)')
+ylabel('Amplitude')
+title('LFM')
