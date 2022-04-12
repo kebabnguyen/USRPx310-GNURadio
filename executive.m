a0ff0746736c27398f4f274125c316f91ea0aa48
kebabnguyen <34109891+kebabnguyen@users.noreply.github.com>
2022-04-12 12:32:53 -0500

    integration process
    need to make a timer or something thatll call the spectrum sampler every so often, and then interface that with the call matlab python block

--- executive.m
+++ executive.m
@@ -1,4 +1,4 @@
-function [thechosen] = executive()
+function [thechosen] = executive(filename)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 sec1 = [6:10 12:22];
@@ -6,7 +6,7 @@ sec2 = [23:24 26:31 33:38 40:41];
 sec3 = [43:52 54:59];
 pilot = [11 25 39 53];
 secs = [sec1; sec2; sec3]; %900 MHz
-occbins = ppbin_v4("readingcenter91M64bins.txt",4, 3);
+occbins = ppbin_v4(filename,4, 3);
 unocc = sectionscroll(occbins,secs);
 thechosen = sectionchooser(unocc);
 end
--- sectionchooser.m
+++ sectionchooser.m
@@ -3,6 +3,7 @@ function chosensect = sectionchooser(freesections)
 %   but what happens if there aren't any?
 if(isempty(freesections))
     chosensect = "plot twist, there was nothing!";
+    chosensect = 1;
     %if there's no free sections, then what do?
     %in our simulated world this wouldn't happen,
     %but i think we'd just generate narrower sections (although with ofdm i
@@ -10,7 +11,12 @@ if(isempty(freesections))
     %since we're simulating the entire thing then this shouldn't happen
     %def a part to improve on; just working on handling this would take a
     %big chunk of time
+    %to make things work we still need to pick a pattern
+    %for starters we'll pick the first one, then if time allows I'll modify
+    %it to choose the one that's the least interfering
 else
-    chosensect = freesections(1,:);
+    chosensect = 0;
+    %matlab arrays go from 1 to n, but python stuff goes from 0 to n-1
+    %bandaid fix would be to just hardcode and spit out 0
 end
 end
